90bcc7c8182b06a4328ba3518b82f2fc5368b1d7
Yusuph Mkangara <yusuph.mka@outlook.com>
2022-04-03 01:31:15 -0700

    Day08 pt 2

--- advent-2020/src/day08.rs
+++ advent-2020/src/day08.rs
@@ -7,7 +7,12 @@ pub fn day08_1(input: &str) -> i32 {
 }
 
 pub fn day08_2(input: &str) -> i32 {
-    0
+    /*
+    Naive implementation says go through and find all the indexes
+    where jmp and nop appear.
+    For each of those indexes, make the switch and run the whole program again
+    */
+    swap_doo(input)
 }
 
 fn split_operation_argument(input: &str) -> [String; 2] {
@@ -39,30 +44,74 @@ fn return_accumulator(input: &str) -> i32 {
     // We don't need to iterate everything, we just need to give it a start until we get false
     while set.insert(idx) && idx < instructions.len() {
         let [operation, argument] = instructions.index(idx);
-        match operation.as_str() {
-            "acc" => {
-                acc += argument
-                    .parse::<i32>()
-                    .expect("Argument should be parseable as a signed integer");
-                idx += 1;
-            }
-            "jmp" => {
-                let movement = argument
-                    .parse::<i32>()
-                    .expect("Argument should be parseable as a signed integer");
+        hozier_move(operation, argument, &mut idx, &mut acc);
+    }
+    acc
+}
 
-                if movement.is_negative() {
-                    idx -= movement.abs() as usize
+// Worth re-reading in the future: https://www.snoyman.com/blog/2020/05/no-mutable-parameters-in-rust/
+fn hozier_move(op: &str, argument: &str, idx: &mut usize, acc: &mut i32) {
+    match op {
+        "acc" => {
+            *acc += argument
+                .parse::<i32>()
+                .expect("Argument should be parseable as a signed integer");
+            *idx += 1;
+        }
+        "jmp" => {
+            let movement = argument
+                .parse::<i32>()
+                .expect("Argument should be parseable as a signed integer");
+
+            if movement.is_negative() {
+                *idx -= movement.abs() as usize
+            } else {
+                *idx += movement.abs() as usize;
+            }
+        }
+        "nop" => {
+            *idx += 1;
+        }
+        _ => {
+            panic!("Shouldn't have made it here");
+        }
+    }
+}
+
+fn swap_doo(input: &str) -> i32 {
+    // looking for index one-less than len()
+    // find all of jmp and nop (maybe easier to just find non-acc)
+    // for each of those indexes, take the instructions and for the special index switch what you do
+    let mut acc = 0;
+    let mut idx: usize = 0;
+    let mut set: HashSet<usize> = HashSet::new();
+    let instructions = create_all_operations(input);
+    let non_acc: Vec<usize> = instructions
+        .iter()
+        .enumerate()
+        .filter(|(_, it)| it[0] != "acc")
+        .map(|x| x.0)
+        .collect();
+
+    for change in non_acc {
+        while set.insert(idx) && idx < instructions.len() {
+            let [operation, argument] = instructions.index(idx);
+            if idx == change {
+                if operation == "nop" {
+                    hozier_move("jmp", argument, &mut idx, &mut acc);
                 } else {
-                    idx += movement.abs() as usize;
+                    hozier_move("nop", argument, &mut idx, &mut acc);
                 }
+            } else {
+                hozier_move(operation.as_str(), argument, &mut idx, &mut acc);
             }
-            "nop" => {
-                idx += 1;
-            }
-            _ => {
-                panic!("Shouldn't have made it here");
-            }
+        }
+        if idx == instructions.len() {
+            break;
+        } else {
+            acc = 0;
+            idx = 0;
+            set.clear();
         }
     }
     acc
@@ -134,9 +183,26 @@ mod tests {
 
     #[test]
     fn test_parsing_positive_negative_to_int() {
+        // Apparently atoi works too here? pretty rad
         let input = "-99";
         let parsed = input.parse::<i32>().unwrap();
         assert_eq!(parsed, -99);
         assert_eq!("+99".parse::<i32>().unwrap(), 99);
+    }
+
+    #[test]
+    fn test_part_02() {
+        let input = r"nop +0
+        acc +1
+        jmp +4
+        acc +3
+        jmp -3
+        acc -99
+        acc +1
+        jmp -4
+        acc +6";
+
+        let actual = swap_doo(input);
+        assert_eq!(actual, 8);
     }
 }
