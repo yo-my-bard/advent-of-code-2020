03d8a3445f0df294ba37f3ebb2925b09a49d40ef
Yusuph Mkangara <yusuph.mka@outlook.com>
2023-12-03 01:42:09 -0800

    This was awful and it's only day1b

--- 2023/day01/src/main.rs
+++ 2023/day01/src/main.rs
@@ -1,3 +1,5 @@
+use phf::phf_map;
+
 const TEST_INPUT: &str = r#"
 1abc2
 pqr3stu8vwx
@@ -5,20 +7,123 @@ a1b2c3d4e5f
 treb7uchet
 "#;
 
+const TEST_INPUT_B: &str = r#"
+two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen
+"#;
+
+const WORD_DIGITS_MAP: phf::Map<&str, &str> = phf_map! {
+    "one" => "1",
+    "two" => "2",
+    "three" => "3",
+    "four" => "4",
+    "five" => "5",
+    "six" => "6",
+    "seven" => "7",
+    "eight" => "8",
+    "nine" => "9",
+};
+
 fn extract_digits_from_line(l: &str) -> i32 {
     let first_digit = l.chars().find(char::is_ascii_digit).expect("should exist");
-    let second_digit = l.chars().rev().find(char::is_ascii_digit).expect("should exist");
+    let second_digit = l
+        .chars()
+        .rev()
+        .find(char::is_ascii_digit)
+        .expect("should exist");
     let num_str = String::new() + &*first_digit.to_string() + &*second_digit.to_string();
-
     num_str.parse::<i32>().unwrap()
+}
+
+fn advanced_extract_digits_from_line(l: &str) -> i32 {
+    let first_digit = bad_but_simple_get_first_digit(l);
+    let second_digit = bad_but_simple_get_second_digit(l);
+    (first_digit + second_digit.as_str())
+        .parse::<i32>()
+        .unwrap()
+}
+
+fn bad_but_simple_get_first_digit(l: &str) -> String {
+    let first_digit_idx = l.find(|c: char| c.is_ascii_digit()).unwrap_or(usize::MAX);
+    let first_word_idx = WORD_DIGITS_MAP
+        .keys()
+        .map(|w| {
+            let x = l.find(w).unwrap_or(usize::MAX);
+            (x, w)
+        })
+        .reduce(|acc, e| if acc.0 <= e.0 { acc } else { e })
+        .unwrap();
+    if first_word_idx.0 <= first_digit_idx {
+        WORD_DIGITS_MAP.get(first_word_idx.1).unwrap().to_string()
+    } else {
+        l.chars().nth(first_digit_idx).unwrap().to_string()
+    }
+}
+
+fn bad_but_simple_get_second_digit(l: &str) -> String {
+    let second_digit_idx = l.rfind(|c: char| c.is_ascii_digit()).unwrap_or(usize::MAX);
+    let second_word_idx = WORD_DIGITS_MAP
+        .keys()
+        .map(|w| {
+            let x = l.rfind(w).unwrap_or(usize::MAX);
+            (x, w)
+        })
+        .reduce(|acc, e| {
+            if acc.0 != usize::MAX && acc.0 > e.0 {
+                acc
+            } else if e.0 != usize::MAX {
+                e
+            } else {
+                acc
+            }
+        })
+        .unwrap();
+
+    // println!(
+    //     "WORD {:?} --- {:?} --- {:?}",
+    //     l, second_digit_idx, second_word_idx
+    // );
+
+    let last_word = WORD_DIGITS_MAP
+        .get(second_word_idx.1)
+        .unwrap_or(&"0")
+        .to_string();
+    let last_digit = l.chars().nth(second_digit_idx).unwrap_or('0').to_string();
+
+    if usize::MAX == second_word_idx.0 {
+        last_digit
+    } else if second_word_idx.0 > second_digit_idx {
+        last_word
+    } else if second_digit_idx != usize::MAX {
+        last_digit
+    } else {
+        last_word
+    }
 }
 
 fn main() {
     println!(
-        "{:#}",
-        include_str!("day01a.txt")
+        "First Answer: {:#}",
+        include_str!("day01.txt")
             .split_ascii_whitespace()
             .map(extract_digits_from_line)
+            .sum::<i32>()
+    );
+
+    println!(
+        "Second Answer: {:#}",
+        include_str!("day01.txt")
+            .split_ascii_whitespace()
+            .map(advanced_extract_digits_from_line)
+            // .map(|i| {
+            //     println!("{:?}", i);
+            //     i
+            // })
             .sum::<i32>()
     )
 }
@@ -35,8 +140,24 @@ mod test {
     }
 
     #[test]
-    fn test_test_input() {
-        let result: i32 = TEST_INPUT.split_ascii_whitespace().map(extract_digits_from_line).sum();
+    fn test_test_input_a() {
+        let result: i32 = TEST_INPUT
+            .split_ascii_whitespace()
+            .map(extract_digits_from_line)
+            .sum();
         assert_eq!(result, 142)
+    }
+
+    #[test]
+    fn test_test_input_b() {
+        let result: i32 = TEST_INPUT_B
+            .split_ascii_whitespace()
+            .map(advanced_extract_digits_from_line)
+            .map(|i| {
+                println!("{:?}", i);
+                i
+            })
+            .sum();
+        assert_eq!(result, 281);
     }
 }
